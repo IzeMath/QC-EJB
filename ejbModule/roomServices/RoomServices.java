7f9e9493007aba148446cfe8b5d308221dbfd498
IzeMath <32262467+IzeMath@users.noreply.github.com>
2018-01-29 19:07:15 +0100

    room

--- ejbModule/roomServices/RoomServices.java
+++ ejbModule/roomServices/RoomServices.java
@@ -1,6 +1,14 @@
 package roomServices;
 
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.quizcon.Room;
 
 /**
  * Session Bean implementation class RoomServices
@@ -8,11 +16,37 @@ import javax.ejb.Stateless;
 @Stateless
 public class RoomServices implements RoomServicesRemote {
 
-    /**
-     * Default constructor. 
-     */
-    public RoomServices() {
-        // TODO Auto-generated constructor stub
-    }
+	@PersistenceContext(unitName = "QC-JPA")
+	EntityManager em;
+
+	/**
+	 * Default constructor.
+	 */
+	public RoomServices() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void createRoom(final String name, final String password) {
+		int id = 0;
+		do{
+			id = ThreadLocalRandom.current().nextInt(100000, 999999);
+		}while(em.find(Room.class, id) != null);
+		final Room r = new Room(id, name, password);
+		em.persist(r);
+		
+	}
+
+	@Override
+	public void addPlayer(final String name) {
+
+	}
+
+	@Override
+	public List<Room> getListRoom() {
+		final Query query = em.createNamedQuery("getListRoom", Room.class);
+		final List<Room> lr = query.getResultList();
+		return lr;
+	}
 
 }
--- ejbModule/roomServices/RoomServicesRemote.java
+++ ejbModule/roomServices/RoomServicesRemote.java
@@ -1,8 +1,16 @@
 package roomServices;
 
+import java.util.List;
+
 import javax.ejb.Remote;
+
+import org.quizcon.Room;
 
 @Remote
 public interface RoomServicesRemote {
+	
+	public void createRoom(String name, String password);
+	public void addPlayer(String id);
+	public List<Room> getListRoom();
 
 }
