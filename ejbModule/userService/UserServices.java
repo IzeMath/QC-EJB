1814366adb296fedde4beb992d77cecead39f5ee
IzeMath <32262467+IzeMath@users.noreply.github.com>
2018-02-20 16:31:50 +0100

    Stat

--- ejbModule/userService/UserServices.java
+++ ejbModule/userService/UserServices.java
@@ -1,11 +1,15 @@
 package userService;
 
+import java.util.Hashtable;
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.user.Statistique;
 import org.user.Utilisateur;
 
 /**
@@ -35,16 +39,38 @@ public class UserServices implements UserServicesRemote {
 	}
 
 	@Override
-	public void createUser(final String name, final String fname, final String username, final String email, final String password) {
-		final Utilisateur user = new Utilisateur(name, fname, username, email, password);
+	public void createUser(final String username, final String email, final String password) {
+		final Utilisateur user = new Utilisateur(username, email, password);
 		em.persist(user);
 
 	}
 
 	@Override
-	public String test() {
-		// TODO Auto-generated method stub
-		return "TEST";
+	public Hashtable<String, Float> getStats(final Utilisateur user) {
+		final Hashtable<String, Float> htStats = new Hashtable<>();
+		final Utilisateur usrData = em.find(Utilisateur.class, user.getId());
+		float nbQuestionT = 0;
+		float nbReponseT = 0;
+		
+		for (final Statistique st : usrData.getlStat()) {
+			final float nbRep =  st.getNbReponse();
+			final float nbQuest = st.getNbQuestions();
+			htStats.put(st.getTheme(), (nbRep / nbQuest) * 100);
+			nbQuestionT += nbQuest;
+			nbReponseT += nbRep;
+		}
+		htStats.put("Total", (nbReponseT / nbQuestionT) *100 );
+		
+		
+		return htStats;
+	}
+
+	@Override
+	public void createUser(final String username, final String email, final String password, final List<Statistique> lStat) {
+		final Utilisateur user = new Utilisateur(username, email, password);
+		user.setlStat(lStat);
+		em.persist(user);
+		
 	}
 
 }
--- ejbModule/userService/UserServicesRemote.java
+++ ejbModule/userService/UserServicesRemote.java
@@ -1,15 +1,21 @@
 package userService;
 
+import java.util.Hashtable;
+import java.util.List;
+
 import javax.ejb.Remote;
 
+import org.user.Statistique;
 import org.user.Utilisateur;
 
 @Remote
 public interface UserServicesRemote {
 
 	public Utilisateur login(String email, String password);
-	void createUser(String name, String fname, String username, String email, String password);
-	public String test();
+	public void createUser(String username, String email, String password);
+	public void createUser(final String username, final String email, final String password, List<Statistique> lStat);
+	public Hashtable<String, Float> getStats(Utilisateur user);
+	
 	
 
 }
